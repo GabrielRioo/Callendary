81f88e4e569be57d17d591492a4e065b951cd6d0
GabrielRioo <gabrielpaivario@gmail.com>
2023-08-09 19:51:11 -0300

    Adding the user on database using Prisma

---
+++ src/pages/api/users/index.api.ts
@@ -0,0 +1,35 @@
+import { prisma } from '@/lib/prisma'
+import { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'POST') {
+    return res.status(405).end()
+  }
+
+  const { name, username } = req.body
+
+  const userExists = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (userExists) {
+    return res.status(400).json({
+      message: 'Username already taken.',
+    })
+  }
+
+  // Cadastrar no banco de dados
+  const user = await prisma.user.create({
+    data: {
+      name,
+      username,
+    },
+  })
+
+  return res.status(201).json(user)
+}
